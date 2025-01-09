949ec66bf0464482d8c2e6605e13a4c70b877c57
UestliGuci <uestli.guci@cit.edu.al>
2025-01-09 01:19:16 +0100

    v1.1.17

--- js/ui.js
+++ js/ui.js
@@ -589,7 +589,7 @@ class UIManager {
                     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                         <path d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
                     </svg>
-                    <span>Swipe left to edit or delete</span>
+                    <span>Swipe left to delete</span>
                 </div>
 
                 ${Object.entries(groupedTransactions).map(([date, dayTransactions]) => `
@@ -646,12 +646,6 @@ class UIManager {
                                         </span>
                                     </div>
                                     <div class="col-actions">
-                                        <button class="btn-edit" onclick="balanceManager.showEditModal('${t.id}')">
-                                            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                                                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                                                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                                            </svg>
-                                        </button>
                                         <button class="btn-delete" onclick="balanceManager.showDeleteConfirmation('${t.id}')">
                                             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                                                 <polyline points="3 6 5 6 21 6"></polyline>
